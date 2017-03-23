fe53c78113d98328cf03df54525d99a97ee140ef
luciorq <luciorqueiroz@gmail.com>
2017-03-23 16:19:14 +0000

    Merged Development unit

--- start_container.sh
+++
@@ -1,7 +0,0 @@
-# Start RStudio server tidyverse container for project dsRBP
-IMAGE_NAME=luciorq/$(basename $(pwd))
-## -t for [tag]
-docker build -t $IMAGE_NAME .
-## -e for setting root
-## -v or --volume for copying folder content
-docker run -d -p 8787:8787 -e ROOT=TRUE --volume $(pwd):/home/rstudio/code $IMAGE_NAME
